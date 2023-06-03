b040145f24a74e5a5711ca380c7255022f84d527
Sarah <dousse.sarah@gmail.com>
2023-06-03 16:50:18 +0100

    initial commit

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,20 @@
+ public class Lasagna {
+    // TODO: define the 'expectedMinutesInOven()' method
+     public int expectedMinutesInOven() {
+         return 40;
+     }
+
+    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int actualMinutesInOven) {
+         return this.expectedMinutesInOven() - actualMinutesInOven;
+    }
+    // TODO: define the 'preparationTimeInMinutes()' method
+     public int preparationTimeInMinutes(int numberOfLayers) {
+         return numberOfLayers * 2;
+     }
+
+    // TODO: define the 'totalTimeInMinutes()' method
+     public int totalTimeInMinutes(int numberOfLayers, int actualMinutesInOven) {
+         return this.preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+     }
+}
