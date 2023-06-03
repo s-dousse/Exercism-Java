683744da1b24ed43bed1d4ad3bd187cf657c86e7
Sarah <dousse.sarah@gmail.com>
2023-06-03 18:40:23 +0100

    Two-fer: apply mentor's suggestions (Remove unnecessary  variable and refactoring)

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,11 +1,10 @@
 public class Twofer {
-    private boolean likesCookies = true;
     public String twofer(String name, boolean likesCookies) {
-        String cookieEater = (likesCookies) && !(name == null) ? name : "you";
+        String cookieEater = likesCookies && name != null ? name : "you";
         return String.format("One for %s, one for me.", cookieEater );
     }
 
     public String twofer(String name) {
-        return twofer(name, likesCookies);
+        return twofer(name, true);
     }
 }
