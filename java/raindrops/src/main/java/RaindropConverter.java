76eaa4a6dfd5807160d0a280be8a4fc8a13a3250
Sarah <dousse.sarah@gmail.com>
2023-07-06 16:03:28 +0100

    Raindrops: use StringBuilder - refactoring

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,11 +1,11 @@
 class RaindropConverter {
 
     String convert(int number) {
-        String result = "";
-        if (number % 3 == 0) { result += "Pling"; }
-        if (number % 5 == 0) { result += "Plang"; }
-        if (number % 7 == 0) { result += "Plong"; }
-        if (result.isEmpty()){result = String.valueOf(number);}
-        return result;
+        StringBuilder rainSong = new StringBuilder();
+        if (number % 3 == 0) { rainSong.append("Pling"); }
+        if (number % 5 == 0) { rainSong.append("Plang"); }
+        if (number % 7 == 0) { rainSong.append("Plong"); }
+        if (rainSong.length() == 0) {rainSong.append(number);}
+        return rainSong.toString();
     }
 }
