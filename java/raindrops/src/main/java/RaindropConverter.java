d95f7ebe4144e9f6a8fc900212a7e736ab4fd5a3
Sarah <dousse.sarah@gmail.com>
2023-07-06 15:55:57 +0100

    Raindrops: remove redundant if statements - refactoring

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -2,23 +2,10 @@ class RaindropConverter {
 
     String convert(int number) {
         String result = "";
-        if(number % 105 == 0) {
-            result = "PlingPlangPlong";
-        } else if(number % 15 == 0) {
-            result = "PlingPlang";
-        } else if(number % 21 == 0) {
-            result = "PlingPlong";
-        } else if(number % 35 == 0) {
-            result = "PlangPlong";
-        } else if(number % 3 == 0) {
-            result = "Pling";
-        } else if(number % 5 == 0) {
-            result = "Plang";
-        } else if(number % 7 == 0) {
-            result = "Plong";
-        } else {
-            result = String.valueOf(number);
-        }
+        if (number % 3 == 0) { result += "Pling"; }
+        if (number % 5 == 0) { result += "Plang"; }
+        if (number % 7 == 0) { result += "Plong"; }
+        if (result.isEmpty()){result = String.valueOf(number);}
         return result;
     }
 }
