283d38d0c6b617de75c8615debaedf1c43e700df
Sarah <dousse.sarah@gmail.com>
2023-07-06 13:22:13 +0100

    Cars assemble: refactoring

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -2,23 +2,19 @@ public class CarsAssemble {
     private final int CARS_PER_HOUR_RATE = 221;
 
     public double productionRatePerHour(int speed) {
+        int producedCars = speed * CARS_PER_HOUR_RATE;
         switch (speed) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-                return speed * CARS_PER_HOUR_RATE;
             case 5:
             case 6:
             case 7:
             case 8:
-                return  speed * CARS_PER_HOUR_RATE * 0.9;
+                return  producedCars * 0.9;
             case 9:
-                return speed * CARS_PER_HOUR_RATE * 0.8;
+                return producedCars * 0.8;
             case 10:
-                return speed * CARS_PER_HOUR_RATE * 0.77;
+                return producedCars * 0.77;
             default:
-                return speed * CARS_PER_HOUR_RATE;
+                return producedCars;
         }
     }
 
